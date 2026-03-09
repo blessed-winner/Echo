bbdf87fe0dd355d1f562f76dfa7cdf3af651d331
Winner <blessedwinner66@gmail.com>
2026-03-09 21:31:34 +0200

    Creation of role enum

--- src/main/java/org/xenon/knowspace/filters/JwtAuthenticationFilter.java
+++ src/main/java/org/xenon/knowspace/filters/JwtAuthenticationFilter.java
@@ -1,4 +1,9 @@
 package org.xenon.knowspace.filters;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
 public class JwtAuthenticationFilter {
 }
