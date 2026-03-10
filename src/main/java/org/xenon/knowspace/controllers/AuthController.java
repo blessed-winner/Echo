b56ae5cf87f50d8b7aa7b0baddb0af829bcc887e
Winner <blessedwinner66@gmail.com>
2026-03-10 21:25:47 +0200

    Defined the login endpoint

--- src/main/java/org/xenon/knowspace/controllers/AuthController.java
+++ src/main/java/org/xenon/knowspace/controllers/AuthController.java
@@ -1,5 +1,50 @@
 package org.xenon.knowspace.controllers;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xenon.knowspace.config.JwtConfig;
+import org.xenon.knowspace.dtos.JwtResponse;
+import org.xenon.knowspace.dtos.LoginRequest;
+import org.xenon.knowspace.repositories.UserRepository;
+import org.xenon.knowspace.services.JwtService;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
 public class AuthController {
+    private UserRepository userRepository;
+    private final JwtService jwtService;
+    private final JwtConfig jwtConfig;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest request, HttpServletResponse response){
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    request.getEmail(),
+                    request.getPassword()
+            );
+
+            var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+
+            var accessToken = jwtService.generateAccessToken(user);
+            var refreshToken = jwtService.generateRefreshToken(user);
+
+            var cookie = new Cookie("refreshToken", refreshToken.toString());
+            cookie.setHttpOnly(true);
+            cookie.setPath("/auth");
+            cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
+            cookie.setSecure(false);
+
+            response.addCookie(cookie);
+
+            return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
+    }
+
 }
