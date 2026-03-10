54c3426cd77bb187b81a0cd700f09e98d67d41a9
Winner <blessedwinner66@gmail.com>
2026-03-10 16:33:01 +0200

    Definition of user service

---
+++ src/main/java/org/xenon/knowspace/services/UserService.java
@@ -0,0 +1,21 @@
+package org.xenon.knowspace.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.xenon.knowspace.repositories.UserRepository;
+
+import java.util.Collections;
+
+public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        var user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return new User(
+                user.getEmail(),
+                user.getName(),
+                Collections.emptyList()
+        );
+    }
+}
