24f9f82d2a5a20b5a655e604574290bffc2a6d34
Winner <blessedwinner66@gmail.com>
2026-03-10 13:50:31 +0200

    Starting jwt service and dealing with claims

--- src/main/java/org/xenon/knowspace/services/JwtService.java
+++ src/main/java/org/xenon/knowspace/services/JwtService.java
@@ -1,4 +1,18 @@
 package org.xenon.knowspace.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
+import org.xenon.knowspace.config.JwtConfig;
+
+@AllArgsConstructor
 public class JwtService {
+    private final JwtConfig jwtConfig;
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .verifyWith(jwtConfig.getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
