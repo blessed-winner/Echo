db9c076cb89faac324c69dee5958ef78f82b8634
Winner <blessedwinner66@gmail.com>
2026-03-10 21:07:47 +0200

    Created relevant classes for auth requests

---
+++ src/main/java/org/xenon/knowspace/dtos/JwtResponse.java
@@ -0,0 +1,9 @@
+package org.xenon.knowspace.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class JwtResponse {
+    private String token;
+}
---
+++ src/main/java/org/xenon/knowspace/dtos/LoginRequest.java
@@ -0,0 +1,6 @@
+package org.xenon.knowspace.dtos;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+}
