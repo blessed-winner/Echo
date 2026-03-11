8321583bf09787cbc4b9e2008bf3230f4e40de8b
Winner <blessedwinner66@gmail.com>
2026-03-11 16:14:13 +0200

    Defined the register user request

--- src/main/java/org/xenon/knowspace/controllers/UserController.java
+++ src/main/java/org/xenon/knowspace/controllers/UserController.java
@@ -1,6 +1,8 @@
 package org.xenon.knowspace.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.xenon.knowspace.repositories.UserRepository;
@@ -10,4 +12,9 @@ import org.xenon.knowspace.repositories.UserRepository;
 @AllArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<?> register(){
+
+    }
 }
