7a7e1d3ee1a9179a49d53bcdbc13581f7048c8e4
Winner <blessedwinner66@gmail.com>
2026-04-02 12:55:49 +0200

    Preparing a user service for modular structure

--- src/main/java/org/xenon/knowspace/services/UserService.java
+++ src/main/java/org/xenon/knowspace/services/UserService.java
@@ -5,7 +5,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.xenon.knowspace.mappers.UserMapper;
 import org.xenon.knowspace.repositories.UserRepository;
 
 import java.util.Collections;
@@ -13,7 +15,11 @@ import java.util.Collections;
 @Service
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
         var user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User not found"));
         return new User(
@@ -22,4 +28,6 @@ public class UserService implements UserDetailsService {
                 Collections.emptyList()
         );
     }
+
+
 }
