1576f50186227c7e25ba969d0b54e7ac3e50c877
Winner <blessedwinner66@gmail.com>
2026-04-04 20:17:52 +0200

    Addition of register user request body format with validation

---
+++ src/main/java/org/xenon/knowspace/dtos/RegisterUserRequest.java
@@ -0,0 +1,22 @@
+package org.xenon.knowspace.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6 ,max = 25, message = "Password must be between 6 and 25 characters")
+    private String password;
+}
