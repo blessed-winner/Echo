459680087a3c7ccd66ef34917aa58546febc93f8
Winner <blessedwinner66@gmail.com>
2026-04-06 14:03:07 +0200

    Integrated the get functionality for batch-user-fetch in the controller

--- src/main/java/org/xenon/knowspace/controllers/UserController.java
+++ src/main/java/org/xenon/knowspace/controllers/UserController.java
@@ -1,14 +1,29 @@
 package org.xenon.knowspace.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.xenon.knowspace.dtos.UserDto;
+import org.xenon.knowspace.entities.Role;
+import org.xenon.knowspace.services.UserService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
-    public ResponseEntity<List<UserDto>> getAllUsers() {}
+    private final UserService userService;
+    public ResponseEntity<List<UserDto>> getAllUsers(
+            @RequestParam(required = false) Role role
+    ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UUID myId = (UUID) authentication.getPrincipal();
+        return ResponseEntity.ok(userService.getAllUsers(role, myId));
+    }
 }
