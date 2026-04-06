0696e1c5aeb8f4b6cf2a43d86d1396e85aee47e5
Winner <blessedwinner66@gmail.com>
2026-04-06 13:48:48 +0200

    Addition of new method for user filtering

--- src/main/java/org/xenon/knowspace/repositories/UserRepository.java
+++ src/main/java/org/xenon/knowspace/repositories/UserRepository.java
@@ -1,12 +1,16 @@
 package org.xenon.knowspace.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.xenon.knowspace.entities.Role;
 import org.xenon.knowspace.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+
+    List<User> findByRoleAndIdNot(Role role, UUID excludeUserId);
 }
--- src/main/java/org/xenon/knowspace/services/UserService.java
+++ src/main/java/org/xenon/knowspace/services/UserService.java
@@ -1,8 +1,23 @@
 package org.xenon.knowspace.services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.xenon.knowspace.dtos.UserDto;
+import org.xenon.knowspace.entities.Role;
+import org.xenon.knowspace.entities.User;
+import org.xenon.knowspace.repositories.UserRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class UserService {
-
+  private final UserRepository userRepository;
+  private List<User> users;
+  public List<UserDto> getAllUsers(Role role, UUID excludeUserId){
+      if(role != null){
+          users = userRepository.findByRoleAndIdNot(role,excludeUserId);
+      }
+  }
 }
