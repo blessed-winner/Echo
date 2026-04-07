4c56bb0c9447be601d4eeb6963bf69c42a8ec2d8
Winner <blessedwinner66@gmail.com>
2026-04-07 12:58:29 +0200

    Addition of the memory-items controller

---
+++ src/main/java/org/xenon/knowspace/controllers/MemoryItemController.java
@@ -0,0 +1,11 @@
+package org.xenon.knowspace.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/memory-items")
+@AllArgsConstructor
+public class MemoryItemController {
+}
