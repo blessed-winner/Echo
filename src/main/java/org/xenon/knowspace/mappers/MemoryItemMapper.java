a51b6d5f2f108afc0d336db5519ff88918dd0fc8
Winner <blessedwinner66@gmail.com>
2026-04-07 13:28:21 +0200

    Populated the mapper for memory items

--- src/main/java/org/xenon/knowspace/mappers/MemoryItemMapper.java
+++ src/main/java/org/xenon/knowspace/mappers/MemoryItemMapper.java
@@ -1,7 +1,12 @@
 package org.xenon.knowspace.mappers;
 
 import org.mapstruct.Mapper;
+import org.xenon.knowspace.dtos.MemoryItemDto;
+import org.xenon.knowspace.dtos.MemoryItemRequest;
+import org.xenon.knowspace.entities.MemoryItem;
 
 @Mapper(componentModel = "spring")
 public interface MemoryItemMapper {
+    MemoryItemDto toDto(MemoryItem memoryItem);
+    MemoryItem toEntity(MemoryItemRequest request);
 }
