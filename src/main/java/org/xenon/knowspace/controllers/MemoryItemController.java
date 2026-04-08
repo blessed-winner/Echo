1b0574c86650b170c7234475fb395f398efb2351
Winner <blessedwinner66@gmail.com>
2026-04-08 12:38:07 +0200

    Endpoint for memory item creation finished

--- src/main/java/org/xenon/knowspace/controllers/MemoryItemController.java
+++ src/main/java/org/xenon/knowspace/controllers/MemoryItemController.java
@@ -1,11 +1,24 @@
 package org.xenon.knowspace.controllers;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xenon.knowspace.dtos.MemoryItemDto;
+import org.xenon.knowspace.dtos.MemoryItemRequest;
+import org.xenon.knowspace.services.MemoryItemService;
 
 @RestController
-@RequestMapping("/memory-items")
+@RequestMapping("/memories")
 @AllArgsConstructor
 public class MemoryItemController {
+    private final MemoryItemService memoryItemService;
+    @PostMapping
+    public ResponseEntity<MemoryItemDto> createMemoryItem(@Valid @RequestBody MemoryItemRequest request){
+        var result = memoryItemService.createMemoryItem(request);
+        return ResponseEntity.ok().body(result);
+    }
 }
