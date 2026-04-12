010d9e4f64b23b8430a8a8d5110f5aee90b1b69d
Winner <blessedwinner66@gmail.com>
2026-04-12 11:35:42 +0200

    Feat:Create note service

--- src/main/java/org/xenon/knowspace/mappers/NoteMapper.java
+++ src/main/java/org/xenon/knowspace/mappers/NoteMapper.java
@@ -2,10 +2,11 @@ package org.xenon.knowspace.mappers;
 
 import org.mapstruct.Mapper;
 import org.xenon.knowspace.dtos.NoteDto;
+import org.xenon.knowspace.dtos.NoteRequest;
 import org.xenon.knowspace.entities.Note;
 
 @Mapper(componentModel = "spring")
 public interface NoteMapper {
     NoteDto toDto(Note note);
-    Note toEntity(NoteDto noteDto);
+    Note toEntity(NoteRequest noteDto);
 }
--- src/main/java/org/xenon/knowspace/services/NoteService.java
+++ src/main/java/org/xenon/knowspace/services/NoteService.java
@@ -5,17 +5,53 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.xenon.knowspace.dtos.NoteDto;
 import org.xenon.knowspace.dtos.NoteRequest;
+import org.xenon.knowspace.entities.Note;
+import org.xenon.knowspace.entities.Tag;
 import org.xenon.knowspace.entities.User;
+import org.xenon.knowspace.exceptions.ForbiddenException;
+import org.xenon.knowspace.exceptions.UserNotFoundException;
+import org.xenon.knowspace.mappers.NoteMapper;
+import org.xenon.knowspace.repositories.NoteRepository;
+import org.xenon.knowspace.repositories.TagRepository;
+import org.xenon.knowspace.repositories.TopicRepository;
+import org.xenon.knowspace.repositories.UserRepository;
 
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class NoteService {
-    private UUID getCurrentUser(){
-        return (UUID) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    private final UserRepository userRepository;
+    private final NoteMapper noteMapper;
+    private final TopicRepository topicRepository;
+    private final NoteRepository noteRepository;
+    private final TagRepository tagRepository;
+    private User getCurrentUser(){
+        return (User) SecurityContextHolder.getContext().getAuthentication();
     }
     public NoteDto createNote(NoteRequest noteRequest){
-        UUID userId = getCurrentUser();
+        User currentUser = getCurrentUser();
+        User user = userRepository.findById(currentUser.getId()).orElseThrow(()->new UserNotFoundException("User not found"));
+        var topic = topicRepository.findById(noteRequest.getTopicId()).orElseThrow();
+        if(!topic.getUser().getId().equals(currentUser.getId())){
+            throw new ForbiddenException("Cannot add note to this topic");
+        }
+        Note note = noteMapper.toEntity(noteRequest);
+        note.setTopic(topic);
+        Set<Tag> tags = new HashSet<>();
+        for(Long tagId : noteRequest.getTagIds()){
+            Tag tag = tagRepository.findById(tagId).orElseThrow(()->new RuntimeException("Tag Not Found"));
+            if(!tag.getUser().getId().equals(currentUser.getId())){
+                throw new ForbiddenException("Cannot add this tag to note" + tag.getName());
+            }
+            tags.add(tag);
+        }
+
+        note.setTags(tags);
+        noteRepository.save(note);
+
+        return noteMapper.toDto(note);
+
     }
 }
