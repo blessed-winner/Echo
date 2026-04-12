2fb65d4b2f00c854ac1ba783d622ef365fe8013a
Winner <blessedwinner66@gmail.com>
2026-04-12 13:04:20 +0200

    Feat:Note summary dto

--- src/main/java/org/xenon/knowspace/repositories/NoteRepository.java
+++ src/main/java/org/xenon/knowspace/repositories/NoteRepository.java
@@ -3,10 +3,27 @@ package org.xenon.knowspace.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.xenon.knowspace.entities.Note;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface NoteRepository extends JpaRepository<Note,Long> {
     Page<Note> findAllByUserId(UUID userId, Pageable pageable);
+
+    @Query("""
+    SELECT COUNT(m) FROM MemoryItem m WHERE m.note.id = :noteId AND m.note.topic.user.id = :userId
+    """)
+    Long countAllItemsByNoteIdAndUserId(Long noteId, UUID userId);
+
+    @Query("""
+    SELECT COUNT(m) FROM MemoryItem m WHERE m.note.id = :noteId AND m.note.topic.user.id = :userId AND m.nextReviewDate <= :now
+    """)
+    Long countDueItemsByNoteIdAndUserId(Long noteId, UUID userId, LocalDateTime now);
+
+    @Query("""
+     SELECT COUNT(m) FROM MemoryItem m WHERE m.note.id = :noteId AND m.note.topic.user.id = :userId AND m.lastReviewed = :now
+    """)
+    Long countReviewedTodayByNoteIdAndUserId(Long noteId, UUID userId, LocalDateTime now);
 }
--- src/main/java/org/xenon/knowspace/services/NoteService.java
+++ src/main/java/org/xenon/knowspace/services/NoteService.java
@@ -115,5 +115,6 @@ public class NoteService {
         noteRepository.delete(note);
     }
 
+    public
 
 }
