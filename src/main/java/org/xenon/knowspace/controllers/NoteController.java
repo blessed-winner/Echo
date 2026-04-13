7b9f45a8ca4d62a8725de3bba951d02f8d70a76f
Winner <blessedwinner66@gmail.com>
2026-04-13 11:15:19 +0200

    Feat:Addition of topic service and controller

--- src/main/java/org/xenon/knowspace/controllers/NoteController.java
+++ src/main/java/org/xenon/knowspace/controllers/NoteController.java
@@ -74,7 +74,7 @@ public class NoteController {
             @RequestParam String query,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
-    ) {
+    ){
         return ResponseEntity.ok(noteService.searchNotes(query, page, size));
     }
 }
---
+++ src/main/java/org/xenon/knowspace/services/TopicService.java
@@ -0,0 +1,7 @@
+package org.xenon.knowspace.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+}
