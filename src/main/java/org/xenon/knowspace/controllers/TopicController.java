3cd9fc2c4577fa4b24877634435b856f458d87d1
Winner <blessedwinner66@gmail.com>
2026-04-13 11:46:36 +0200

    Feat:Controller method for topic creation

--- src/main/java/org/xenon/knowspace/controllers/TopicController.java
+++ src/main/java/org/xenon/knowspace/controllers/TopicController.java
@@ -1,10 +1,29 @@
 package org.xenon.knowspace.controllers;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.xenon.knowspace.dtos.TopicDto;
+import org.xenon.knowspace.dtos.TopicRequest;
+import org.xenon.knowspace.entities.Topic;
+import org.xenon.knowspace.services.TopicService;
 
 @RestController
 @RequestMapping("/topics")
 @AllArgsConstructor
 public class TopicController {
+    private final TopicService topicService;
+    @PostMapping
+    public ResponseEntity<TopicDto> createTopic(
+            @Valid @RequestBody TopicRequest request,
+            UriComponentsBuilder uriBuilder
+    ){
+        TopicDto result = topicService.createTopic(request);
+        var uri = uriBuilder.path("/topics/{id}").buildAndExpand(result).toUri();
+        return ResponseEntity.created(uri).body(result);
+    }
 }
