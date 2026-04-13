3c8fab11dca616fdeb712766a477ce0e1841569b
Winner <blessedwinner66@gmail.com>
2026-04-13 11:42:23 +0200

    Feat:Service method for topic creation

--- src/main/java/org/xenon/knowspace/services/TopicService.java
+++ src/main/java/org/xenon/knowspace/services/TopicService.java
@@ -1,19 +1,24 @@
 package org.xenon.knowspace.services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.xenon.knowspace.dtos.TopicDto;
 import org.xenon.knowspace.dtos.TopicRequest;
 import org.xenon.knowspace.entities.Topic;
+import org.xenon.knowspace.entities.User;
 import org.xenon.knowspace.exceptions.ForbiddenException;
+import org.xenon.knowspace.exceptions.UserNotFoundException;
 import org.xenon.knowspace.mappers.TopicMapper;
 import org.xenon.knowspace.repositories.TopicRepository;
 import org.xenon.knowspace.repositories.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class TopicService {
   private final TopicRepository topicRepository;
   private final TopicMapper topicMapper;
@@ -25,9 +30,15 @@ public class TopicService {
 
   public TopicDto createTopic(TopicRequest topicRequest){
       UUID userId = getCurrentUser();
+      User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found"));
       Optional<Topic> existingTopic = topicRepository.findByNameIgnoreCaseAndUserId(topicRequest.getName(), userId);
       if(existingTopic.isPresent()){
             throw new IllegalArgumentException("Topic with the same name already exists");
       }
+      var topic = topicMapper.toEntity(topicRequest);
+      topic.setUser(user);
+      topic.setCreatedAt(LocalDateTime.now());
+
+      return topicMapper.toDto(topicRepository.save(topic));
   }
 }
