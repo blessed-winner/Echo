9b59a805359b68683a373608fb38565fd458f984
Winner <blessedwinner66@gmail.com>
2026-04-20 09:12:04 +0200

    Fix: Allow access at root endpoint

--- src/main/java/org/xenon/echo/config/SecurityConfig.java
+++ src/main/java/org/xenon/echo/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 .sessionManagement(c->c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                         c-> c.requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
+                         c-> c.requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html", "/").permitAll()
                                                            .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
                                                            .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
                                                            .anyRequest().authenticated()
