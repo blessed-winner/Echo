7ddb98265b5c034cf1e63c94132e665ca8e0649e
Winner <blessedwinner66@gmail.com>
2026-04-21 16:36:45 +0200

    Feat: Service for token verification finished

--- src/main/java/org/xenon/echo/services/VerificationTokenService.java
+++ src/main/java/org/xenon/echo/services/VerificationTokenService.java
@@ -48,4 +48,9 @@ public class VerificationTokenService {
 
         return token;
     }
+
+    public void markAsUsed(VerificationToken token){
+        token.setUsedAt(LocalDateTime.now());
+        verificationTokenRepository.save(token);
+    }
 }
