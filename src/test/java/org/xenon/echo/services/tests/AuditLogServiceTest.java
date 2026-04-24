275f47b5ad78323b28ff5872998c1d45dc7d69f2
Winner <blessedwinner66@gmail.com>
2026-04-24 14:20:52 +0200

    Feat: Auditlog service test

--- src/test/java/org/xenon/echo/services/tests/AuditLogServiceTest.java
+++ src/test/java/org/xenon/echo/services/tests/AuditLogServiceTest.java
@@ -1,9 +1,32 @@
 package org.xenon.echo.services.tests;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import org.xenon.echo.enums.AuditAction;
+import org.xenon.echo.repositories.AuditLogRepository;
+import org.xenon.echo.services.AuditLogService;
+
+import java.util.UUID;
 
 public class AuditLogServiceTest {
     @Test
     void shouldSaveAuditLogWithCorrectData(){
+        AuditLogRepository repo = mock(AuditLogRepository.class);
+        AuditLogService service = new AuditLogService(repo);
 
+        UUID userId = UUID.randomUUID();
+        AuditAction action = AuditAction.LOGIN_SUCCESS;
+        String ip = "127.0.0.1";
+
+        service.log(userId,action,ip,true,null,"meta");
+        verify(repo).save(argThat(log->
+            log.getUserId() == userId &&
+            log.getAction() == action &&
+            log.getIpAddress().equals(ip) &&
+            log.isSuccess() &&
+            log.getFailureReason() == null &&
+            log.getMetadata().equals("meta")&&
+            log.getTimestamp() != null
+        ));
     }
 }
