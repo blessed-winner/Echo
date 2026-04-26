9f2daac082ca65e574b2c8a6687346f1ac116cb4
Winner <blessedwinner66@gmail.com>
2026-04-26 21:20:57 +0200

    Feat: Test for token validations

--- src/test/java/org/xenon/echo/services/tests/VerificationTokenServiceTest.java
+++ src/test/java/org/xenon/echo/services/tests/VerificationTokenServiceTest.java
@@ -2,11 +2,13 @@ package org.xenon.echo.services.tests;
 
 
 import org.junit.jupiter.api.*;
+import org.xenon.echo.entities.VerificationToken;
 import org.xenon.echo.enums.TokenType;
 import org.xenon.echo.repositories.VerificationTokenRepository;
 import org.xenon.echo.services.VerificationTokenService;
 import org.xenon.echo.utils.TokenUtil;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -32,6 +34,50 @@ public class VerificationTokenServiceTest {
         when(tokenRepo.findByTokenHash("hashed")).thenReturn(Optional.empty());
         assertThrows(RuntimeException.class,()->{
            service.validateToken(rawToken, TokenType.PASSWORD_RESET);
+        });
+    }
+
+    @Test
+    void shouldThrowWhenTokenAlreadyUsed(){
+        String rawToken = "used-token";
+
+        VerificationToken token = new VerificationToken();
+        token.setUsedAt(LocalDateTime.now());
+
+        when(tokenUtil.hashToken(any())).thenReturn("hashed");
+        when(tokenRepo.findByTokenHash("hashed")).thenReturn(Optional.of(token));
+
+        assertThrows(RuntimeException.class, ()->{
+            service.validateToken(rawToken, TokenType.PASSWORD_RESET);
+        });
+    }
+
+    @Test
+    void shouldThrowWhenTokenExpired(){
+        String rawToken = "expired-token";
+
+        VerificationToken token = new VerificationToken();
+        token.setExpiresAt(LocalDateTime.now().minusDays(1));
+
+        when(tokenUtil.hashToken(any())).thenReturn("hashed");
+        when(tokenRepo.findByTokenHash("hashed")).thenReturn(Optional.of(token));
+
+        assertThrows(RuntimeException.class, ()->{
+            service.validateToken(rawToken, TokenType.PASSWORD_RESET);
+        });
+    }
+
+    @Test
+    void shouldThrowWhenTokenTypeInvalid(){
+        String rawToken = "invalid-token";
+
+        VerificationToken token = new VerificationToken();
+        token.setTokenType(TokenType.EMAIL_VERIFY);
+
+        when(tokenUtil.hashToken(any())).thenReturn("hashed");
+        when(tokenRepo.findByTokenHash("hashed")).thenReturn(Optional.of(token));
+        assertThrows(RuntimeException.class, ()->{
+            service.validateToken(rawToken, TokenType.PASSWORD_RESET);
         });
     }
 }
