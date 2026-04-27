e7294c713161e33dca4a3fc09e64e9db002304d6
Winner <blessedwinner66@gmail.com>
2026-04-27 13:04:29 +0200

    Change of profile status

--- src/test/java/org/xenon/echo/services/tests/integration/AuthIntegrationTest.java
+++ src/test/java/org/xenon/echo/services/tests/integration/AuthIntegrationTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@ActiveProfiles("active")
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 public class AuthIntegrationTest {
         @Autowired
