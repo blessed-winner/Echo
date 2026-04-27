d10e0a15dc274b85f9662bb5d7d368ab1e981c29
Winner <blessedwinner66@gmail.com>
2026-04-27 11:49:23 +0200

    Feat: Integration test for user verification

--- src/test/java/org/xenon/echo/services/tests/integration/AuthIntegrationTest.java
+++ src/test/java/org/xenon/echo/services/tests/integration/AuthIntegrationTest.java
@@ -1,15 +1,22 @@
 package org.xenon.echo.services.tests.integration;
 
+import io.jsonwebtoken.security.Password;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.xenon.echo.entities.User;
+import org.xenon.echo.enums.Role;
 import org.xenon.echo.repositories.UserRepository;
+import org.xenon.echo.services.EmailService;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,6 +29,12 @@ public class AuthIntegrationTest {
 
         @Autowired
         private UserRepository userRepository;
+
+        @Autowired
+        private PasswordEncoder passwordEncoder;
+
+        @MockitoBean
+        private EmailService emailService;
 
         @BeforeEach
         void setup(){
@@ -48,5 +61,29 @@ public class AuthIntegrationTest {
             assertNotNull(user);
             assertEquals("test@mail.com",user.getEmail());
             assertFalse(user.isVerified());
+            verify(emailService).sendVerificationEmail(any(),any());
+        }
+
+        @Test
+        void shouldBlockLoginWhenUserNotVerified() throws Exception{
+            User user = new User();
+            user.setEmail("test@mail.com");
+            user.setPassword(passwordEncoder.encode("Password123"));
+            user.setVerified(false);
+            user.setRole(Role.USER);
+
+            userRepository.save(user);
+
+            String request = """
+              {
+              "email":"test@mail.com",
+              "password":"Password123"
+              }
+            """;
+            mockMvc.perform(
+                    post("/auth/login")
+                            .contentType("application/json")
+                            .content(request)
+            ).andExpect(status().isBadRequest());
         }
 }
