fa1913205e5a7016cf4bc9a973e6ca96a5f3a1c3
Winner <blessedwinner66@gmail.com>
2026-04-30 20:37:32 +0200

    Functionality to get views per item

--- src/main/java/org/xenon/echo/services/ReviewService.java
+++ src/main/java/org/xenon/echo/services/ReviewService.java
@@ -2,20 +2,36 @@ package org.xenon.echo.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.xenon.echo.dtos.ReviewDto;
+import org.xenon.echo.entities.Review;
 import org.xenon.echo.exceptions.MemoryItemNotFoundException;
+import org.xenon.echo.mappers.ReviewMapper;
 import org.xenon.echo.repositories.MemoryItemRepository;
 import org.xenon.echo.repositories.ReviewRepository;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final MemoryItemRepository memoryItemRepository;
+    private final ReviewMapper reviewMapper;
 
-    public Page<ReviewDto> getReviewsPerItem(Long id){
+    private UUID getCurrentUser(){
+        return (UUID) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public Page<ReviewDto> getReviewsPerItem(Long id,int page, int size){
+        UUID userId = getCurrentUser();
        var memoryItem = memoryItemRepository.findById(id).orElseThrow(()->new MemoryItemNotFoundException("Memory Item not found"));
-
+        Pageable pageable = PageRequest.of(page,size, Sort.by("reviewDate").descending());
+       Page<Review> reviews = reviewRepository.findByMemoryItemIdAndMemoryItemUserId(memoryItem.getId(),userId,pageable);
+       return reviews.map(reviewMapper::toDto);
     }
 }
