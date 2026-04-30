6f9024c0939b97f199a189cd081d4a1e5e849ca9
Winner <blessedwinner66@gmail.com>
2026-04-30 20:43:07 +0200

    Correction of parameter problems in review method

--- src/main/java/org/xenon/echo/controllers/MemoryItemController.java
+++ src/main/java/org/xenon/echo/controllers/MemoryItemController.java
@@ -100,6 +100,6 @@ public class MemoryItemController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ){
-        return ResponseEntity.ok(reviewService.getReviewsPerItem(id));
+        return ResponseEntity.ok(reviewService.getReviewsPerItem(id,page,size));
     }
 }
