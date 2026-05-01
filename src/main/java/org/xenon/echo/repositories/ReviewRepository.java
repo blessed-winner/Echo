d6b2eb24dfdaa63bdf47def93ab0f3fc4dd3be63
Winner <blessedwinner66@gmail.com>
2026-05-01 19:52:37 +0200

    Addition of necessary methods for review stats

--- src/main/java/org/xenon/echo/repositories/ReviewRepository.java
+++ src/main/java/org/xenon/echo/repositories/ReviewRepository.java
@@ -3,10 +3,26 @@ package org.xenon.echo.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.xenon.echo.entities.Review;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
     Page<Review> findByMemoryItemIdAndMemoryItemUserId(Long memoryItemId, UUID userId, Pageable pageable);
+    @Query("""
+      SELECT COUNT(r) FROM Review r WHERE r.memoryItem.user.id =: userId
+     """)
+    long countByUserId(UUID userId);
+
+    @Query("""
+      SELECT COUNT(r) FROM Review r WHERE r.memoryItem.user.id =:userId AND r.reviewDate >=: startOfDay
+    """)
+    long countToday(UUID userId, LocalDateTime startOfDay);
+
+    @Query("""
+     SELECT COUNT(r) FROM Review r WHERE r.memoryItem.user.id =:userId AND r.reviewDate >=:startOfWeek
+    """)
+    long countReviewsThisWeek(UUID userId, LocalDateTime startOfWeek);
 }
