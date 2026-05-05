b4ff1915e36552ea301f5560f4c4651220b4b8de
Winner <blessedwinner66@gmail.com>
2026-05-05 16:49:53 +0200

    Dedicated success handler for oauth2 authentication(beginning

---
+++ src/main/java/org/xenon/echo/services/Oauth2SuccessHandler.java
@@ -0,0 +1,7 @@
+package org.xenon.echo.services;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+}
