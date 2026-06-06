66f9391b1dc29ef383a6a62c2e87d1c95f426bde
Winner <blessedwinner66@gmail.com>
2026-06-06 11:58:44 +0200

    Notification type enum

--- src/main/java/org/xenon/echo/entities/Notification.java
+++ src/main/java/org/xenon/echo/entities/Notification.java
@@ -1,19 +1,43 @@
 package org.xenon.echo.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.xenon.echo.enums.NotificationStatus;
+import org.xenon.echo.enums.NotificationType;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String message;
 
+    @Enumerated(EnumType.STRING)
     private NotificationType type;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus status;
+
+    private boolean read;
+
+    private UUID referenceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User recipient;
 }
