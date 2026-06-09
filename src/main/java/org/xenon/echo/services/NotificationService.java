f7b35720e475da3aa5370f8c45b90e269f65eb1c
Winner <blessedwinner66@gmail.com>
2026-06-09 13:46:08 +0200

    Utility to create notification

--- src/main/java/org/xenon/echo/services/NotificationService.java
+++ src/main/java/org/xenon/echo/services/NotificationService.java
@@ -1,62 +1,45 @@
 package org.xenon.echo.services;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.xenon.echo.dtos.NotificationRequest;
 import org.xenon.echo.dtos.NotificationResponse;
 import org.xenon.echo.entities.Notification;
+import org.xenon.echo.entities.User;
 import org.xenon.echo.enums.NotificationStatus;
+import org.xenon.echo.enums.NotificationType;
 import org.xenon.echo.repositories.NotificationRepository;
-
-import java.nio.file.AccessDeniedException;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
-@Transactional
 public class NotificationService {
     private NotificationRepository notificationRepository;
     private SimpMessagingTemplate messagingTemplate;
 
-    public UUID getCurrentUser() {
-        return (UUID) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
-
-    public NotificationResponse createNotification(
-            NotificationRequest request
-    ) {
+    public Notification createNotification(
+            User recipient,
+            String title,
+            String message,
+            NotificationType type
+    ){
         Notification notification = Notification.builder()
-                .recipient(request.getRecipient())
-                .title(request.getTitle())
-                .message(request.getMessage())
-                .type(request.getType())
-                .status(NotificationStatus.DELIVERED)
+                .recipient(recipient)
+                .title(title)
+                .message(message)
+                .type(type)
+                .status(NotificationStatus.SENT)
                 .read(false)
                 .build();
 
         var saved = notificationRepository.save(notification);
         push(saved);
-        return NotificationResponse.builder()
-                .id(saved.getId())
-                .title(saved.getTitle())
-                .message(saved.getMessage())
-                .type(saved.getType())
-                .createdAt(saved.getCreatedAt())
-                .read(saved.isRead())
-                .referenceId(saved.getReferenceId())
-                .build();
+        return saved;
+
     }
 
     public void push(
             Notification notification
-    ) {
+    ){
         NotificationResponse response = NotificationResponse.builder()
                 .id(notification.getId())
                 .title(notification.getTitle())
@@ -64,43 +47,11 @@ public class NotificationService {
                 .type(notification.getType())
                 .createdAt(notification.getCreatedAt())
                 .read(notification.isRead())
-                .referenceId(notification.getReferenceId())
                 .build();
 
         messagingTemplate.convertAndSend(
-                "/topic/users/" + notification.getRecipient().getId(),
+                "topic/users/" + notification.getRecipient().getId(),
                 response
         );
-    }
-
-    @Transactional(readOnly = true)
-    public long countUnread() {
-        UUID userId = getCurrentUser();
-        return notificationRepository.countByRecipientIdAndReadFalse(userId);
-    }
-
-    public void markAsRead(UUID id) throws AccessDeniedException {
-        UUID userId = getCurrentUser();
-        var notification = notificationRepository.findById(id).orElseThrow(() -> new RuntimeException("Notification not found"));
-        if (!notification.getRecipient().getId().equals(userId)) {
-            throw new AccessDeniedException("Forbidden");
-        }
-        notification.setRead(true);
-    }
-
-    @Transactional(readOnly = true)
-    public Page<NotificationResponse> getMyNotifications(int page, int size) {
-        UUID userId = getCurrentUser();
-        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
-        Page<Notification> myNotifications = notificationRepository.findByRecipientIdOrderByCreatedAtDesc(userId, pageable);
-        return myNotifications.map(notification -> NotificationResponse.builder()
-                .id(notification.getId())
-                .title(notification.getTitle())
-                .message(notification.getMessage())
-                .type(notification.getType())
-                .createdAt(notification.getCreatedAt())
-                .read(notification.isRead())
-                .referenceId(notification.getReferenceId())
-                .build());
     }
 }
