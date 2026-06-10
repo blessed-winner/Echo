6297eb93d5e3da809432b0b9511400ca08de14ba
Winner <blessedwinner66@gmail.com>
2026-06-10 14:34:20 +0200

    Notification methods implemented in controller(get-all,mark_as_read,count_unread)

--- src/main/java/org/xenon/echo/controllers/NotificationController.java
+++ src/main/java/org/xenon/echo/controllers/NotificationController.java
@@ -2,13 +2,16 @@ package org.xenon.echo.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.xenon.echo.dtos.NotificationRequest;
+import org.xenon.echo.dtos.NotificationResponse;
 import org.xenon.echo.entities.Notification;
 import org.xenon.echo.services.NotificationService;
+
+import java.util.UUID;
 
 @Tag(name = "Notifications")
 @RestController
@@ -22,5 +25,24 @@ public class NotificationController {
             NotificationRequest request
     ){
         return ResponseEntity.ok(notificationService.createNotification(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<NotificationResponse>>findMyNotifications(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        return ResponseEntity.ok(notificationService.getMyNotifications(page,size));
+    }
+
+    @GetMapping("/unread-count")
+    public ResponseEntity<Long> countUnread(){
+        return ResponseEntity.ok(notificationService.countUnread());
+    }
+
+    @PostMapping("/{id}/read")
+    public ResponseEntity<Void>markAsRead(@PathVariable UUID id){
+        notificationService.markAsRead(id);
+        return ResponseEntity.ok().build();
     }
 }
