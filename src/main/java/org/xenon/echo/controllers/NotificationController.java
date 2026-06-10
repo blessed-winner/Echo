ad1d84caa14673225381d12a4f9add400b33361a
Winner <blessedwinner66@gmail.com>
2026-06-10 14:13:21 +0200

    Function to count unread and marking notification as read in the notification service

--- src/main/java/org/xenon/echo/controllers/NotificationController.java
+++ src/main/java/org/xenon/echo/controllers/NotificationController.java
@@ -1,17 +1,23 @@
 package org.xenon.echo.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.xenon.echo.dtos.NotificationRequest;
 import org.xenon.echo.entities.Notification;
 import org.xenon.echo.services.NotificationService;
 
+@Tag(name = "Notifications")
 @RestController
 @AllArgsConstructor
+@RequestMapping("/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
 
+    @PostMapping
     public ResponseEntity<Notification> createNotification(
             NotificationRequest request
     ){
