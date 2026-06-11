01b73e11e5e9ec3c2f3d3a5be9b75ca3a8a7b4c3
Winner <blessedwinner66@gmail.com>
2026-06-11 07:22:58 +0200

    Scheduler configuration

--- src/main/java/org/xenon/echo/repositories/NotificationRepository.java
+++ src/main/java/org/xenon/echo/repositories/NotificationRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.xenon.echo.entities.Notification;
+import org.xenon.echo.enums.NotificationStatus;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     Page<Notification> findByRecipientIdOrderByCreatedAtDesc(UUID userId, Pageable pageable);
     Long countByRecipientIdAndReadFalse(UUID userId);
+    Page<Notification> findByStatusAndDeliverAtBefore(NotificationStatus status, Instant time, Pageable pageable);
 }
