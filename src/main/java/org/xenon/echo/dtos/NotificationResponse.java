921bd445c9f792ec8878a63cfc7be551753571b8
Winner <blessedwinner66@gmail.com>
2026-06-19 08:26:51 +0200

    Addition of getters to notification response

--- src/main/java/org/xenon/echo/dtos/NotificationResponse.java
+++ src/main/java/org/xenon/echo/dtos/NotificationResponse.java
@@ -2,12 +2,14 @@ package org.xenon.echo.dtos;
 
 
 import lombok.Builder;
+import lombok.Getter;
 import org.xenon.echo.enums.NotificationType;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Builder
+@Getter
 public class NotificationResponse {
     private UUID id;
     private String title;
